c419052b768861941f6050b9c1a905ecd3e13907
Tim Caswell <tim@creationix.com>
2017-06-27 17:09:16 -0500

    Add arm support

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 local names = {
   ["OSX-x64"] = "luatweetnacl.so",
   ["Linux-x64"] = "luatweetnacl.so",
+  ["Linux-arm"] = "luatweetnacl.so",
 }
 local ffi = require 'ffi'
 local arch = ffi.os .. "-" .. ffi.arch
