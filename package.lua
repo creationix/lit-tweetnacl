5adfa717148f83d97a8be3554e35ca9ebba7041a
Tim Caswell <tim@creationix.com>
2017-06-27 17:13:22 -0500

    First public release

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
   name = "creationix/tweetnacl",
-  version = "0.0.2",
+  version = "1.0.1",
   homepage = "https://github.com/creationix/littweetnacl",
   description = "luatweetnacl bindings packaged for lit applications.",
   tags = {"nacl","crypto"},
